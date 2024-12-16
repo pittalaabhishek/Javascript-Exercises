1c93f8d2ade2ff9984265be077aac105cc1cd0fd
VenkatAbhi <pittalaabhishek999@gmali.com>
2024-12-16 10:16:39 +0530

    Fixed a bug in module introduction

--- Module Introduction/index.js
+++ Module Introduction/index.js
@@ -3,7 +3,7 @@ let button = document.getElementById("showMessageButton");
 let message = document.getElementById("message");
 
 button.addEventListener("click", function() {
-    if (message.style.display === "none") { //We do this so that we can toggle between showing and hiding the message on the screen
+    if (message.style.display === "none" || message.style.display === "") { //We do this so that we can toggle between showing and hiding the message on the screen
         message.style.display = "block"; // Show the message
     } else {
         message.style.display = "none"; // Hide the message
